d9564aed49558d16e7546425c94b92834bc4d595
Joao.F <113448443+Cannedsans@users.noreply.github.com>
2022-11-01 19:59:35 -0300

    Create calc.cpp

---
+++ c++/calc.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+	float nota_1,nota_2,calc_media;
+	nota_1 = 10;
+	nota_2 = 7;
+	calc_media = (nota_1 + nota_2 ) / 2 ;
+	printf("o resultado e = %f", calc_media);
+	getch ();   //esperar tecla
+}
+
