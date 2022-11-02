8203ba19e709bc9b8f85170ef45655ffd9983ec1
Joao.F <113448443+Cannedsans@users.noreply.github.com>
2022-11-02 09:13:31 -0300

    Create aula2.py

---
+++ pyton/aula2.py
@@ -0,0 +1,6 @@
+// variavéis, para definir variavéis não é nescessario definir seu tipo, porém toda variavél tbm é um OBJETO, isso será visto no futuro
+nome = 'joao'
+idade = 16
+peso = 75.5
+//o uso da virgula é mais apropriado para esse caso pois ai ele não irá somar o peso e a idade
+print (nome, idade, peso)
