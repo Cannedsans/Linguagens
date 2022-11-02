10fd480dd3720cc36af721a5dd58f9d0d6d03abc
Joao.F <113448443+Cannedsans@users.noreply.github.com>
2022-11-02 09:31:25 -0300

    Create ler.py

---
+++ pyton/ler.py
@@ -0,0 +1,6 @@
+//para ler uma variavél ao invés de por o nome da variavél depois do "=" coloca o "input" e em seguida o texto para exigir a var.
+nome = input ("qual é o seu nome?")
+idade = input ("qual é a sua idade?")
+peso =  input ("quanto vc pesa?")
+
+print = (nome, idade , peso)
