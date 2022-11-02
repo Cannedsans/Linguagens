c3440e4c925da10e2fe2b9ec80136ff7396365c8
Joao.F <113448443+Cannedsans@users.noreply.github.com>
2022-11-01 21:11:17 -0300

    Create aula1.py

---
+++ pyton/aula1.py
@@ -0,0 +1,5 @@
+print ("olá mundo") //tanto faz serem aspas simples ou compostas 
+
+print (7+4) // o resultado será o número 11 e nesse caso não precisa de aspas
+
+print ("7"+"4") // esse caso vai escrever 74 pois o + vai juntar o que foi escrito em cada aspas e o + pode ser separado por virgul 
