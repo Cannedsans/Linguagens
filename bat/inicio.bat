478b36a9e4b717b86cb256357fafb242a2b86d71
Joao.F <113448443+Cannedsans@users.noreply.github.com>
2022-11-03 20:58:21 -0300

    Create inicio.bat

---
+++ bat/inicio.bat
@@ -0,0 +1,3 @@
+//em geral os comandos em bat são executados no terminal porém podem ser executadas no terminal, porem arquivos .bat podem ser criados para executar códigos como qualquer outro comando
+echo oi // o comando echo mostra o que está escrito em seguida 
+pause // o comando pause serve no formato de arquivo para o termianl não fechar assim que o comando fosse executado 
