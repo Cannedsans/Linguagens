cc280fcb824b0147ca049bd9ce39bacfc7c1c3c6
Joao.F <113448443+Cannedsans@users.noreply.github.com>
2022-11-03 20:32:43 -0300

    Update terminal.rb

--- ruby/terminal.rb
+++ ruby/terminal.rb
@@ -1,2 +1,3 @@
 // o comando irb
 irb // esse comando faz com que não seja nescessario criar um arquivo .rb e executar os códigos direto no terminal  
+exit // e para sair dessa função e volatar para o terminal normal usa o exit
