ad0d9f6b085c0e78664e76aecbd891b1a1821d9a
Joao.F <113448443+Cannedsans@users.noreply.github.com>
2022-11-04 10:51:56 -0300

    Create variaveis.rb

---
+++ ruby/variaveis.rb
@@ -0,0 +1,3 @@
+nome = 'jonas' //para definir as variavéis é nescessario colocar o nome ou caractere dentro das aspas porém caso sejam números não é nescessario  
+puts nome //para imprimir a variavél apenas a escreva ao lado do "puts" 
+jonas // saida do comadno "puts"
