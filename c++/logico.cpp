5809988b21e8dfdd8629bea09a44a2fa576dd2ab
Joao.F <113448443+Cannedsans@users.noreply.github.com>
2022-12-19 08:30:22 -0300

    Create logico.cpp

---
+++ c++/logico.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main (){
+   float n=0;// O valor númerico entre 1 e 0 tem valores lógicos o valor 1 é verdadeiro e o 0 é falso 
+   if (n){ //Os operadores lógicos ! é o não, and é o E e o or é o OU e funciona em todas as linguagens porém o ! pode não ser o não   
+    printf ("verdadeiro");
+   }else{
+    printf ("falso");
+   }
+}
