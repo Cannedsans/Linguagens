878fb330f8e37ae7ed099f8d8b667cb89a16739a
Joao.F <113448443+Cannedsans@users.noreply.github.com>
2022-12-22 09:35:14 -0300

    Create OperadorTernario.cpp

---
+++ c++/OperadorTernario.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main (){
+   float n=2,o;
+   
+   (n == 0) ? o = 0: o = 1;// o operador ternario é um IF simplificado em que após a operação se coloca o "?" em seguida oq fazer quando for verdadeiro 
+  // e oq fazer quando for falso após o ":" é um IF mais rápido porém não é possivél fazer ele ser mais complexo como imprimir algo na tela etc.
+   
+   printf ("%f",o);
+}
