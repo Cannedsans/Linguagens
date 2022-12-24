65661aee72f9191f76e7c21ea872ac20d9587c47
Joao.F <113448443+Cannedsans@users.noreply.github.com>
2022-12-24 10:28:12 -0300

    Create while.cpp

---
+++ c++/while.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;// nescessariio par fazer ocout funcionar 
+
+int main (){
+	int n=0;
+	while(n<11){ // o while é um laço de repetição ele irá executar repetidas vezes até a expressão dele ser falsa (nesse caso o código vai contar de zero até 10 
+        cout << n << "\n"; // usei no lugar do printf pq achei mias fácil de usar 
+		n++;
+	}
+}
