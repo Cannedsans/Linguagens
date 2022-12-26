7ac7ef93e95e7cbf5ec155d14b55b8432230c68a
Joao.F <113448443+Cannedsans@users.noreply.github.com>
2022-12-26 10:38:47 -0300

    Create For.cpp

---
+++ c++/For.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main (){ // o for é outro laço de repetção esse é melhor para usar matrizes e vetors 
+	for (int i=0//,x=1; i< 11; i++,x+=2){ //o legal é que dá para colocar mais de uma variavél dentro dele 
+	cout << i << "\n" //<< -- << x;
+	}
+}
+   
