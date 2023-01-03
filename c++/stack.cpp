db871c8144c730cab51279ff97c9013b9db5d3d1
Joao.F <113448443+Cannedsans@users.noreply.github.com>
2023-01-03 09:52:05 -0300

    Create stack.cpp

---
+++ c++/stack.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stack> // esse comando spo funcioa apartir de uma biblioteca 
+using namespace std;
+
+int main (){
+	
+	stack <string> cartas; // as pilhas são como vetores verticais só que há uma ordem para os valores como se estivessem empilhados um em cima do outro 
+	
+	cartas.push("copas"); // o comando push serve para adicionar os valores a pilha 
+	cartas.push("espadas");
+	cartas.push("ouro");
+	cartas.push("paus");
+	
+	cout << "tem, " << cartas.size() << " cartas no baralho\n"; //size para ver o tamanho da pilha 
+	
+	cout << "vai puxar o .:" << cartas.top() << "\n"; //top vai indicar o valor no topo da pilha (o ultimo valor a ser adicionado)
+	
+	cartas.pop(); //pop vai retirar o valor do topo
+	
+	cout << "agora tem, " << cartas.size() << " cartas no baralho\n";
+	
+	cout << "vai puxar o .:" << cartas.top() << "\n";
+}
