2a36b13e8df2217d1001276ef74c7ab50fdc5b8c
Joao.F <113448443+Cannedsans@users.noreply.github.com>
2023-01-04 10:27:22 -0300

    Update stack.cpp

--- c++/stack.cpp
+++ c++/stack.cpp
@@ -6,6 +6,10 @@ int main (){
 	
 	stack <string> cartas; // as pilhas são como vetores verticais só que há uma ordem para os valores como se estivessem empilhados um em cima do outro 
 	
+	if (cartas.empty()){ // empty é um comando para verificar se há objetos na pilha 
+		cout << "ainda não há cartas\n\n";
+	}
+	
 	cartas.push("copas"); // o comando push serve para adicionar os valores a pilha 
 	cartas.push("espadas");
 	cartas.push("ouro");
@@ -20,4 +24,10 @@ int main (){
 	cout << "agora tem, " << cartas.size() << " cartas no baralho\n";
 	
 	cout << "vai puxar o .:" << cartas.top() << "\n";
+	
+	while (!cartas.empty()){
+		cartas.pop();
+	}
+	
+	cout << "agora tem, " << cartas.size() << " cartas no baralho\n";
 }
